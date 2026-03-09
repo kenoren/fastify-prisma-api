aedfb2ec89287eb7166d83eb6d53288d279d31e9
Mathis Fourey <mfourey@info.local>
2026-03-09 10:43:56 +0100

    create user model

--- src/server.ts
+++ src/server.ts
@@ -7,63 +7,78 @@ app.get("/posts", async () => {
     return prisma.post.findMany()
 })
 
-app.get("/posts/:id", async (req, res) => {
-    const { id } = req.params as any
+app.get("/posts/:id", async (request, reply) => {
+    const { id } = request.params as any
 
     const post = await prisma.post.findUnique({
         where: { id: Number(id) }
     })
 
     if (!post) {
-        res.code(404)
+        reply.code(404)
         return { error: "Post not found" }
     }
-    res.code(200)
+    reply.code(200)
     return post
 })
 
-app.put("/posts/:id", async (req, res) => {
-    const { id } = req.params as any
-    const { title, content } = req.body as any
+app.put("/posts/:id", async (request, reply) => {
+    const { id } = request.params as any
+    const { title, content } = request.body as any
 
     try {
         const post = await prisma.post.update({
             where: { id: Number(id) },
             data: { title, content }
         })
-        res.code(200)
+        reply.code(200)
         return post
     } catch (error) {
-        res.code(404)
+        reply.code(404)
         return { error: "Post not found" }
     }
 })
 
-app.delete("/posts/:id", async (req, res) => {
-    const {id} = req.params as any
+app.delete("/posts/:id", async (request, reply) => {
+    const {id} = request.params as any
 
     try {
         await prisma.post.delete({
             where: {id: Number(id)}
         })
-        res.code(204)
+        reply.code(204)
         return
     } catch (error) {
-        res.code(404)
+        reply.code(404)
         return {error: "Post not found"}
     }
 })
 
 app.post("/posts", async (request, reply) => {
-    const { title, content } = request.body as any
+    const { title, content, authorId } = request.body as any
 
     const post = await prisma.post.create({
-        data: { title, content }
+        data: { title, content, authorId }
     })
 
     reply.code(201)
     return post
 })
+
+app.get("/users", async (request, reply) => {
+    return prisma.user.findMany()
+})
+
+
+app.post("/users", async (request, reply) => {
+    const {login, firstname,lastname} = request.body as any
+    const user = await prisma.user.create({
+        data: {login, firstname,lastname}
+    })
+    reply.code(201)
+    return user
+})
+
 
 app.listen({ port: 3000 }, () => {
     console.log("Server running on http://localhost:3000")
